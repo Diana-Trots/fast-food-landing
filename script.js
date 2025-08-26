ff17f9341bede8262ef956ac9049c70c7c811b06
Diana Trots <dianatrots98@gmail.com>
2025-08-26 23:46:02 +0300

    Адаптация сайта под телефон

---
+++ script.js
@@ -0,0 +1,7 @@
+const menuIcon = document.querySelector(".menu-icon");
+const header = document.querySelector(".header");
+
+menuIcon.addEventListener("click", () => {
+  menuIcon.classList.toggle("menu-icon--active");
+  header.classList.toggle("header--mobile");
+});
